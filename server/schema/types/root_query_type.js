e7566707f60e09044fe9bf1fb17d9368d6d474cf
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-02 01:34:53 -0500

    Mutation to check if a user has been authenticated DONE

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,13 +1,18 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
-
+const UserType = require('./user_type');
 const RootQueryType = new GraphQLObjectType({
 	name: 'RootQueryType',
 	//By default, GraphQl expects to have at least one field
 	//available for every single defined type, therefore
 	//we will add a dummy field so we don't get any errors
 	fields: {
-		dummyField: { type: GraphQLID },
+		currentUser: {
+			type: UserType,
+			resolve (parentValue, args, req) {
+				return req.user;
+			},
+		},
 	},
 });
 
