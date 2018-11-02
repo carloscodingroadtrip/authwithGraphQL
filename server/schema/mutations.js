1945a7b83d58815ed94ace4b11e3ad42c0dfa40a
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-02 01:13:47 -0500

    Sign up mutation has been added

---
+++ server/schema/mutations.js
@@ -0,0 +1,34 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = graphql;
+const AuthService = require('../services/auth');
+const UserType = require('./types/user_type');
+
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		//It should return a user that was created
+		// >>> a UserType
+		signup: {
+			type: UserType,
+			args: {
+				email: { type: GraphQLString },
+				password: { type: GraphQLString },
+			},
+			//now we will do the resolve, and we will pass a third argument
+			// request or context in some other documentation
+			// that is the request object coming from Express
+			resolve (parentValue, { email, password }, req) {
+				//since AuthService returns a promise, and whenever we
+				//return a promise from our resolve function, we have to
+				//make sure to return it so that GraphQL knows to
+				//look at the promise that gets returned, and say
+				//'Ok, I am just gonna hold up for a second and wait for
+				// this operation to resolve before I attempt to return any
+				// value to the front-end"
+				return AuthService.signup({ email, password, req });
+			},
+		},
+	},
+});
+
+module.exports = mutation;
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,14 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+	name: 'RootQueryType',
+	//By default, GraphQl expects to have at least one field
+	//available for every single defined type, therefore
+	//we will add a dummy field so we don't get any errors
+	fields: {
+		dummyField: { type: GraphQLID },
+	},
 });
 
 module.exports = RootQueryType;
