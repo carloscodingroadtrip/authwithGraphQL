ba90a82cf03f1ea90d1c8846f33927b615239f10
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-03 00:26:38 -0500

    UI for login plus catching errors DONE

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 const App = (props) => {
 	return (
-		<div>
+		<div className="container">
 			<Header />
 			{props.children}
 		</div>
---
+++ client/components/AuthForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class AuthForm extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+		};
+	}
+
+	formSubmit (e) {
+		e.preventDefault();
+		const { email, password } = this.state;
+		this.props.onSubmit({ email, password });
+	}
+
+	render () {
+		return (
+			<div className="row">
+				<form onSubmit={this.formSubmit.bind(this)} className="col s6">
+					<div className="input-field">
+						<input
+							placeholder="Email"
+							type="text"
+							value={this.state.email}
+							onChange={(e) => this.setState({ email: e.target.value })}
+						/>
+					</div>
+					<div className="input-field">
+						<input
+							placeholder="Password"
+							type="password"
+							type="text"
+							value={this.state.password}
+							onChange={(e) => this.setState({ password: e.target.value })}
+						/>
+					</div>
+					<div className="errors">{this.props.errors.map((error) => <div key={error}>{error}</div>)}</div>
+					<button className="btn">Submit</button>
+				</form>
+			</div>
+		);
+	}
+}
