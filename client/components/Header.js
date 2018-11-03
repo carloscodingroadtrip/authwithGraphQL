a553e5a24d39facb73a00eee58cb52c4638a4a1c
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-02 22:28:18 -0500

    Auth on the client side is COMPLETE

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
-export default class Header extends Component {
+class Header extends Component {
 	render () {
+		console.log(this.props.data);
 		return <div>Header</div>;
 	}
 }
+
+export default graphql(query)(Header);
