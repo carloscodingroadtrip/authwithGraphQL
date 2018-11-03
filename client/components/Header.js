b5b77dc0dc9ab65447ee103fa1aba27063531618
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-02 22:37:42 -0500

    Sign In and sign out logic COMPLETE

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,9 +3,30 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-	render () {
+	renderButtons () {
 		console.log(this.props.data);
-		return <div>Header</div>;
+
+		const { loading, currentUser } = this.props.data;
+		if (loading) {
+			return <div />;
+		}
+
+		//Do we show the buttons, which ones ?
+		//If user exist
+		if (currentUser) {
+			return <div>Logout</div>;
+		} else {
+			return <div>You are not signed in</div>;
+		}
+	}
+
+	render () {
+		// console.log(this.props.data);
+		return (
+			<nav className="">
+				<div className="nav-wrapper">{this.renderButtons()}</div>
+			</nav>
+		);
 	}
 }
 
