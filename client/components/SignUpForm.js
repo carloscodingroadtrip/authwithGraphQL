cf11baa79c66e8aca6d69100f910267d64ec62f1
Carlos Molina <molina.carlos.dev@gmail.com>
2018-11-04 21:58:17 -0600

    Add UI for sign up form

---
+++ client/components/SignUpForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+
+class SignUp extends Component {
+	render () {
+		return (
+			<div>
+				<h3>Sign up Form </h3>
+				<AuthForm />
+			</div>
+		);
+	}
+}
+
+export default SignUp;
